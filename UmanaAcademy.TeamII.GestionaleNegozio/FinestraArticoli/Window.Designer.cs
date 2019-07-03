8005ffa88d0967af5ff3c00d37ad160206bd816f
jdb-muu <muuumuuu@muccamu.muu>
2019-07-03 16:38:32 +0200

    Ancoraggio datagridView

--- UmanaAcademy.TeamII.GestionaleNegozio/FinestraArticoli/Window.Designer.cs
+++ UmanaAcademy.TeamII.GestionaleNegozio/FinestraArticoli/Window.Designer.cs
@@ -98,9 +98,10 @@
             // 
             this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(9, 6);
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 3);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(1428, 706);
+            this.dataGridView1.Size = new System.Drawing.Size(1444, 712);
             this.dataGridView1.TabIndex = 0;
             this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.DataGridView1_CellContentClick);
             // 
