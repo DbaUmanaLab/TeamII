f35da4e154a7a41ab677628f9176eabcb04f651e
jdb-muu <muuumuuu@muccamu.muu>
2019-07-03 10:50:39 +0200

    PROBVA 4

--- ManagementMarketPlus/PROVA/Program.cs
+++ ManagementMarketPlus/PROVA/Program.cs
@@ -6,7 +6,7 @@ namespace PROVA
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! MUCCA");
         }
     }
 }
