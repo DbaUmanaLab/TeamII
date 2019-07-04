ad983d353d7d7b9d301581ee6aa077340715953d
nixnotnic <jnvvffgfsf@gmail.com>
2019-07-04 12:27:37 +0200

    interface

--- UmanaAcademy.TeamII.GestionaleNegozio/FinestraArticoli/VisualWindow.cs
+++ UmanaAcademy.TeamII.GestionaleNegozio/FinestraArticoli/VisualWindow.cs
@@ -23,6 +23,7 @@ namespace FinestraArticoli
             using (var reader = new StreamReader("Files\\Magazzino\\products.csv"))
             using (var csv = new CsvReader(reader))
             {
+                csv.Configuration.RegisterClassMap<ArticoloMap>();
                 csv.Configuration.Delimiter = ",";
                 csv.Read();
                 articoli = csv.GetRecords<Articolo>().ToList();
